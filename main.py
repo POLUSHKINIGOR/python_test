48461b57975238879e38e307bbb1970d58fef16e
Polushkin Igor <kazpolushkin@gmail.com>
2024-06-18 11:13:21 +0200

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def sayHello():
-    print("Helloo")
-    print("World")
-    print("Vsem Gopa ):")
 
-sayHello()
+x = int(input("Enter a number - "))
+y = int(input("Enter a number - "))
+
+def sum(a,b):
+    return a + b
+print("The sum of the numbers is - ",sum(x,y))
+
